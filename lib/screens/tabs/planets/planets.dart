e5a0e184e596c8ec0d50504add1e539183fdea18
LINUX-PC <jesusrope98@outlook.com>
2018-09-12 17:01:49 +0200

    Updated moon list UI

--- lib/screens/tabs/planets/planets.dart
+++ lib/screens/tabs/planets/planets.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:native_widgets/native_widgets.dart';
 import 'package:scoped_model/scoped_model.dart';
+import 'package:space_news/widgets/head_card_page.dart';
 import 'package:space_news/widgets/hero_image.dart';
 import 'package:space_news/widgets/list_cell.dart';
+import 'package:space_news/widgets/row_item.dart';
 
 import '../../../models/planets/planets.dart';
 import '../../../models/planets/celestial_body.dart';
@@ -59,13 +61,52 @@ class PlanetsHomePage extends StatelessWidget {
       return Center(child: Text('No Moons Found'));
     }
     return ListView.builder(
-        itemCount: moons.length,
-        itemBuilder: (BuildContext context, int index) {
-          return Text(
-            moons[index].name,
-            textAlign: TextAlign.center,
-          );
-        });
+      padding: const EdgeInsets.all(16.0),
+      itemCount: moons.length,
+      itemBuilder: (BuildContext context, int index) {
+        final CelestialBody moon = moons[index];
+        return HeadCardPage(
+          image: HeroImage().buildHero(
+            size: 116.0,
+            context: context,
+            url: moon.imageUrl,
+            tag: moon.id,
+            title: moon.name,
+          ),
+          title: moon.name,
+          subtitle: Column(
+            children: <Widget>[
+              RowItem.textRow('Aphelion', moon.getAphelion),
+              const SizedBox(height: 8.0),
+              RowItem.textRow('Perihelion', moon.getPerihelion),
+              const SizedBox(height: 8.0),
+              RowItem.textRow('Period', moon.getPeriod),
+              const SizedBox(height: 8.0),
+              RowItem.textRow('Speed', moon.getSpeed),
+              const SizedBox(height: 8.0),
+              RowItem.textRow('Inclination', moon.getInclination),
+              const SizedBox(height: 8.0),
+              RowItem.textRow('Radius', moon.getRadius),
+              const SizedBox(height: 8.0),
+              RowItem.textRow('Volume', moon.getVolume),
+              const SizedBox(height: 8.0),
+              RowItem.textRow('Mass', moon.getMass),
+              const SizedBox(height: 8.0),
+              RowItem.textRow('Density', moon.getDensity),
+              const SizedBox(height: 8.0),
+              RowItem.textRow('Gravity', moon.getGravity),
+              const SizedBox(height: 8.0),
+              RowItem.textRow('Escape velocity', moon.getEscapeVelocity),
+              const SizedBox(height: 8.0),
+              RowItem.textRow('Temperature', moon.getTemperature),
+              const SizedBox(height: 8.0),
+              RowItem.textRow('Pressure', moon.getPressure),
+            ],
+          ),
+          details: moon.description,
+        );
+      },
+    );
   }
 
   Widget _buildItem(BuildContext context, int index) {
@@ -91,10 +132,7 @@ class PlanetsHomePage extends StatelessWidget {
                       builder: (BuildContext context) {
                         return SafeArea(
                           child: Container(
-                            child: Padding(
-                              padding: EdgeInsets.only(bottom: 10.0),
-                              child: _buildMoons(context, index, _moons),
-                            ),
+                            child: _buildMoons(context, index, _moons),
                           ),
                         );
                       },
