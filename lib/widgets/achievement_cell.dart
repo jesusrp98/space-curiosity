1493587803a698b528c157bf066defb8020a9640
LINUX-PC <jesusrope98@outlook.com>
2018-10-24 16:26:58 +0200

    Tweaked achiv. cell

--- lib/widgets/achievement_cell.dart
+++ lib/widgets/achievement_cell.dart
@@ -17,44 +17,54 @@ class AchievementCell extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      contentPadding: const EdgeInsets.symmetric(
-        horizontal: 16.0,
-        vertical: 8.0,
-      ),
-      leading: CircleAvatar(
-        radius: 25.0,
-        backgroundColor: Colors.white,
-        child: Text(
-          '#$index',
-          style:
-              Theme.of(context).textTheme.subhead.copyWith(color: Colors.black),
-        ),
-      ),
-      title: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
+      contentPadding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+      title: Row(
         children: <Widget>[
-          Text(
-            title,
-            style: TextStyle(
-              fontSize: 18.0,
-              fontWeight: FontWeight.bold,
-              color: primaryText,
+          CircleAvatar(
+            radius: 25.0,
+            backgroundColor: Colors.white,
+            child: Text(
+              '#$index',
+              style: Theme.of(context)
+                  .textTheme
+                  .subhead
+                  .copyWith(color: Colors.black),
             ),
           ),
-          Text(
-            date,
-            style: Theme.of(context).textTheme.subhead.copyWith(
+          Container(width: 16.0),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text(
+                title,
+                style: TextStyle(
+                  fontSize: 18.0,
+                  fontWeight: FontWeight.bold,
                   color: primaryText,
                 ),
+              ),
+              Text(
+                date,
+                style: Theme.of(context).textTheme.subhead.copyWith(
+                      color: primaryText,
+                    ),
+              ),
+            ],
           ),
-          Container(height: 8.0),
         ],
       ),
-      subtitle: Text(
-        subtitle,
-        textAlign: TextAlign.justify,
-        style:
-            Theme.of(context).textTheme.subhead.copyWith(color: secondaryText),
+      subtitle: Column(
+        children: <Widget>[
+          Container(height: 8.0),
+          Text(
+            subtitle,
+            textAlign: TextAlign.justify,
+            style: Theme.of(context)
+                .textTheme
+                .subhead
+                .copyWith(color: secondaryText),
+          ),
+        ],
       ),
       onTap: () async => await FlutterWebBrowser.openWebPage(
             url: url,
