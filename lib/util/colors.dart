aaa3c9f1bb38ffee5181ccc20b025e88a8076600
LINUX-PC <jesusrope98@outlook.com>
2019-01-03 22:09:29 +0100

    Big color refactor

--- lib/util/colors.dart
+++ lib/util/colors.dart
@@ -2,34 +2,6 @@ import 'package:flutter/material.dart';
 
 /// COLORS FILE
 /// It contains app colors to used them all around the app
-/// 
-/// IDEALLY, IN THIS FILE THERE WOULD BE ONLY 
-/// PRIMARY & ACCENT COLORS, FOR BOTH LIGHT & DARK THEMES.
-/// MAYBE SOME AUX COLORS FOR SMALL WIDGETS ASWELL.
-
-// These should be here, with the light theme's counterparts
-const primaryColor = const Color(0xFF151515);
-const accentColor = const Color(0xFFFFBC02D);
-
-// These would be replace with in-theme colors
-const backgroundColor = const Color(0xFF212121);
-const cardColor = const Color(0xFF2C2C2C);
-
-// This three should go away
-const primaryText = const Color(0xFFFFFFFF);
-const secondaryText = const Color(0xFFBCBCBC);
-const lateralText = const Color(0xFF828282);
-
-// This here are for RowItems, they could stay here
-const acceptIcon = const Color(0xFF4CAF50);
-const denyIcon = const Color(0xFFF44336);
-const nullIcon = const Color(0xFF607D8B);
-
-// Definetly should go away
-const highlightColor = const Color(0xFF424242);
-
-// I think it should go away
-const dividerColor = const Color(0xFF616161);
 
 // Used in maps, could stay here
 const locationPin = const Color(0xFFD32F2F);
@@ -40,7 +12,7 @@ const disabledFab = const Color(0xFF757575);
 /// LIGHT THEME
 const lightPrimaryColor = const Color(0xFF151515);
 const lightAccentColor = const Color(0xFFFFBC02D);
-const lightDividerColor = const Color(0xFF151515);
+const lightDividerColor = const Color(0xFFD5D5D5);
 
 /// DARK THEME
 const darkPrimaryColor = const Color(0xFF151515);
@@ -55,3 +27,8 @@ const blackAccentColor = const Color(0xFFFFBC02D);
 const blackBackgroundColor = const Color(0xFF000000);
 const blackCardColor = const Color(0xFF1A1A1A);
 const blackDividerColor = const Color(0xFF616161);
+
+/// RowItem's icon colors
+const acceptIcon = const Color(0xFF4CAF50);
+const denyIcon = const Color(0xFFF44336);
+const nullIcon = const Color(0xFF607D8B);
