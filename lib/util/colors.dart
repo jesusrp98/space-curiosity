e8f265920c626610bca2ab03b42c572f2d6702d3
LINUX-PC <jesusrope98@outlook.com>
2019-01-09 16:40:31 +0100

    File tweak

--- lib/util/colors.dart
+++ lib/util/colors.dart
@@ -3,12 +3,6 @@ import 'package:flutter/material.dart';
 /// COLORS FILE
 /// It contains app colors to used them all around the app
 
-// Used in maps, could stay here
-const locationPin = const Color(0xFFD32F2F);
-
-// Used for disabled fabs, GO AWAY!
-const disabledFab = const Color(0xFF757575);
-
 /// LIGHT THEME
 const lightPrimaryColor = const Color(0xFF151515);
 const lightAccentColor = const Color(0xFFFFBC02D);
@@ -32,3 +26,9 @@ const blackDividerColor = const Color(0xFF616161);
 const acceptIcon = const Color(0xFF4CAF50);
 const denyIcon = const Color(0xFFF44336);
 const nullIcon = const Color(0xFF607D8B);
+
+// Map's pin color
+const locationPin = const Color(0xFFD32F2F);
+
+// Colors used for diabled FABs
+const disabledFab = const Color(0xFF757575);
