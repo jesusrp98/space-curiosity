b1cc4258f071b42dc55c8179763324f6145327ec
Rody Davis <rdavis@unifycrm.com>
2019-03-04 10:33:25 -0500

    Giving User Ability to Change Colors for Theme

--- lib/models/models.dart
+++ lib/models/models.dart
@@ -1,3 +1,2 @@
 export './articles/post.dart';
-export './app_model.dart';
 export './query_model.dart';
--- lib/ui/settings.dart
+++ lib/ui/settings.dart
@@ -1,43 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_i18n/flutter_i18n.dart';
+import 'package:persist_theme/data/models/theme_model.dart';
+import 'package:persist_theme/persist_theme.dart';
 import 'package:scoped_model/scoped_model.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:space_news/ui/general/separator.dart';
 
-import '../models/app_model.dart';
-
-class SettingsScreen extends StatefulWidget {
-  @override
-  _SettingsScreenState createState() => _SettingsScreenState();
-}
-
-class _SettingsScreenState extends State<SettingsScreen> {
-  bool _darkTheme = false;
-  bool _oledBlack = false;
-
-  @override
-  void initState() {
-    Themes _theme = ScopedModel.of<AppModel>(context)?.theme ?? Themes.dark;
-
-    if (_theme == Themes.light)
-      setState(() {
-        _darkTheme = false;
-        _oledBlack = false;
-      });
-    else if (_theme == Themes.black)
-      setState(() {
-        _darkTheme = true;
-        _oledBlack = true;
-      });
-    else
-      setState(() {
-        _darkTheme = true;
-        _oledBlack = false;
-      });
-
-    super.initState();
-  }
-
+class SettingsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -48,73 +17,41 @@ class _SettingsScreenState extends State<SettingsScreen> {
         )),
         centerTitle: true,
       ),
-      body: ScopedModelDescendant<AppModel>(
-        builder: (context, child, model) => ListView(
-              children: <Widget>[
-                ListTile(
-                  title: Text(FlutterI18n.translate(
-                    context,
-                    'settings.dark_theme.title',
-                  )),
-                  subtitle: Text(FlutterI18n.translate(
-                    context,
-                    'settings.dark_theme.body',
-                  )),
-                  trailing: Switch(
-                    activeColor: Theme.of(context).accentColor,
-                    value: _darkTheme,
-                    onChanged: (value) => _changeTheme(
-                          model: model,
-                          theme: value
-                              ? _oledBlack ? Themes.black : Themes.dark
-                              : Themes.light,
-                        ),
-                  ),
-                ),
-                ListTile(
-                  title: Text(FlutterI18n.translate(
-                    context,
-                    'settings.oled_black.title',
-                  )),
-                  subtitle: Text(FlutterI18n.translate(
-                    context,
-                    'settings.oled_black.body',
-                  )),
-                  trailing: Switch(
-                    activeColor: Theme.of(context).accentColor,
-                    value: _oledBlack,
-                    onChanged: (value) => _changeTheme(
-                          model: model,
-                          theme: value ? Themes.black : Themes.dark,
-                        ),
-                  ),
-                ),
-                Separator.divider(),
-              ],
-            ),
+      body: ListView(
+        children: <Widget>[
+          DarkModeSwitch(
+            title: Text(FlutterI18n.translate(
+              context,
+              'settings.dark_theme.title',
+            )),
+            subtitle: Text(FlutterI18n.translate(
+              context,
+              'settings.dark_theme.body',
+            )),
+          ),
+          TrueBlackSwitch(
+            title: Text(FlutterI18n.translate(
+              context,
+              'settings.oled_black.title',
+            )),
+            subtitle: Text(FlutterI18n.translate(
+              context,
+              'settings.oled_black.body',
+            )),
+          ),
+          CustomThemeSwitch(),
+          PrimaryColorPicker(
+            type: PickerType.material,
+          ),
+          AccentColorPicker(
+            type: PickerType.material,
+          ),
+          DarkAccentColorPicker(
+            type: PickerType.material,
+          ),
+          Separator.divider(),
+        ],
       ),
     );
-  }
-
-  void _changeTheme({AppModel model, Themes theme}) async {
-    final SharedPreferences prefs = await SharedPreferences.getInstance();
-
-    prefs.setInt('theme', theme.index);
-    model.theme = theme;
-    if (theme == Themes.dark)
-      setState(() {
-        _darkTheme = true;
-        _oledBlack = false;
-      });
-    else if (theme == Themes.black)
-      setState(() {
-        _darkTheme = true;
-        _oledBlack = true;
-      });
-    else
-      setState(() {
-        _darkTheme = false;
-        _oledBlack = false;
-      });
   }
 }
