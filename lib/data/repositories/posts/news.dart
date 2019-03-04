662b29c91d464ff03f3b0180503e3bc9873dbf7a
Rody Davis <rdavis@unifycrm.com>
2019-03-04 12:21:01 -0500

    Update news.dart

--- lib/data/repositories/posts/news.dart
+++ lib/data/repositories/posts/news.dart
@@ -10,27 +10,8 @@ class NewsRepo {
   NewsRepo({@required this.httpClient});
 
   Future<List<Post>> fetchPosts(int startIndex, int limit) async {
-    // final response = await httpClient
-    //     .get('https://www.nasa.gov/rss/dyn/breaking_news.rss/$startIndex');
-
-    // if (response.statusCode == 200) {
-    //   var channel = new RssFeed.parse(response.body);
-    //   final data = channel?.items;
-    //   return data.map((rawPost) {
-    //     return Post(
-    //       id: data?.indexOf(rawPost),
-    //       title: rawPost?.title,
-    //       body: rawPost?.description,
-    //       date: rawPost?.pubDate,
-    //       url: rawPost?.link,
-    //     );
-    //   }).toList();
-    // } else {
-    //   throw Exception('error fetching posts');
-    // }
-
     final response = await httpClient.get('https://www.space.com/feeds/all');
-    print("Result:${response.statusCode} => ${response?.body}");
+    // print("Result:${response.statusCode} => ${response?.body}");
 
     try {
       return RssFeed.parse(response.body)
