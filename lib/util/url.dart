574ff6886bdaac8e009f499e1e2dff6d5c06bccc
LINUX-PC <jesusrope98@outlook.com>
2019-03-06 23:03:30 +0100

    Readded urls

--- lib/util/url.dart
+++ lib/util/url.dart
@@ -87,12 +87,23 @@ class Url {
     'https://www.reddit.com/user/SoundDr',
     'https://www.reddit.com/user/jesusrp98'
   ];
+  
+  static const List<String> authorsTwitter = [
+    'https://www.twitter.com/RodyDavis',
+    'https://www.twitter.com/jesusrp98'
+  ];
+  
+  static const List<String> authorsGithub = [
+    'https://github.com/AppleEducate',
+    'https://github.com/jesusrp98'
+  ];
 
   static const String githubPage =
       'https://github.com/jesusrp98/space-curiosity';
   static const String email =
       'mailto:space.curiosity.app@gmail.com?subject=About%20Space%20Curiosity';
   static const String flutterPage = 'https://flutter.dev/';
+
   // NASA APOD service
   static const String dailyPicture = '$nasaBaseUrl?api_key=$nasaApiKey';
   static const String morePictures =
